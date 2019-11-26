e686c4e61cd54e6cac3d81567906fd85e2415d47
Fernando Perez-Garcia <fepegar@gmail.com>
2019-11-26 18:57:50 +0000

    Update resector code

--- resector/resector.py
+++ resector/resector.py
@@ -2,27 +2,37 @@
 
 """Main module."""
 
+import sys
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 from subprocess import call
+import vtk
+from vtk.util.numpy_support import numpy_to_vtk
+from vtk.numpy_interface import dataset_adapter as dsa
+import torch
 import numpy as np
-from tqdm import tqdm, trange
+from tqdm import tqdm
 import SimpleITK as sitk
 from skimage import filters
-from scipy.stats import rayleigh, rice, norm
+from noise import pnoise3, snoise3
 
 # pylint: disable=no-member
 
 
 def resect(
-        input_path,
-        parcellation_path,
-        noise_image_path,
-        output_path,
-        resection_mask_output_path,
-        hemisphere,
-        radius=None,
-        opening_radius=None,
+            input_path,
+            parcellation_path,
+            noise_image_path,
+            output_path,
+            resection_mask_output_path,
+            hemisphere,
+            radius=None,
+            opening_radius=None,
         ):
+    brain = read(input_path)
+    parcellation = read(parcellation_path)
+    noise_image = read(noise_image_path)
+
     if radius is None:
         # From the dataset (although it looks bimodal)
         mean = 10000
@@ -30,87 +40,115 @@ def resect(
         volume = -1
         while volume < 0:
             volume = np.random.normal(mean, std)
-        radius = (3/4 * volume * np.pi)**(1/3)
-    brain = read(input_path)
-    parcellation = read(parcellation_path)
-    gray_matter_mask = get_gray_matter_mask(parcellation, hemisphere)
-    resectable_hemisphere_mask = get_resectable_hemisphere_mask(parcellation, hemisphere)
+        radius = (3 / 4 * volume * np.pi)**(1 / 3)
 
-    resection_mask = get_resection_mask(
+    # Blend
+    if sigmas is None:
+        sigmas = np.random.uniform(low=0.5, high=1, size=3)
+
+    gray_matter_mask = get_gray_matter_mask(parcellation, hemisphere)
+    resectable_hemisphere_mask = get_resectable_hemisphere_mask(
+        parcellation, hemisphere)
+    resected_brain, resection_mask, _ = _resect(
+        brain,
+        gray_matter_mask,
+        resectable_hemisphere_mask,
+        noise_image,
+        radius,
+        sigmas,
+        opening_radius=opening_radius,
+    )
+    write(resected_brain, output_path)
+    write(resection_mask, resection_mask_output_path)
+
+
+def _resect(
+            brain,
+            gray_matter_mask,
+            resectable_hemisphere_mask,
+            noise_image,
+            radius,
+            sigmas,
+            opening_radius=None,
+        ):
+    resection_mask, center_ras = get_resection_mask_from_mesh(
         resectable_hemisphere_mask,
         gray_matter_mask,
         radius,
         opening_radius=opening_radius,
     )
-
-    # Blend
-    sigmas = np.random.uniform(low=0.5, high=1, size=3)
-    noise_image = read(noise_image_path)
     resected_brain = blend(brain, noise_image, resection_mask, sigmas)
-
-    write(resected_brain, output_path)
-    write(resection_mask, resection_mask_output_path)
+    return resected_brain, resection_mask, center_ras
 
 
-def get_resection_mask(
-        resectable_hemisphere_mask,
-        gray_matter_mask,
-        radius,
-        opening_radius=None,
-        method='vtk',
-    ):
-
-    if method == 'vtk':
-        voxel_center = get_random_voxel(gray_matter_mask, border=False)
-        center = gray_matter_mask.TransformIndexToPhysicalPoint(tuple(voxel_center))
-        r, a, s = center
-        center = -r, -a, s  # LPS to RAS
-        reference_path = '/tmp/ref.nii'
-        write(resectable_hemisphere_mask, reference_path)
-        poly_data = get_noisy_sphere_poly_data(
-            center,
+def get_resection_mask_from_mesh(
+            resectable_hemisphere_mask,
+            gray_matter_mask,
             radius,
-        )
-        model_path = '/tmp/model.vtp'
-        import vtk
-        writer = vtk.vtkXMLPolyDataWriter()
-        writer.SetFileName(model_path)
-        writer.SetInputData(poly_data)
-        writer.Write()
-        result_path = '/tmp/result.nii'
-        sphere_mask = mesh_to_volume(reference_path, model_path, result_path)
-        sphere_mask = read(result_path)
-    else:
-        voxel_image = get_random_voxel_image(gray_matter_mask, border=False)
-        sphere_mask = get_sphere_mask(voxel_image, radius)
+            opening_radius=None,
+        ):
+    """
+    TODO: figure out how to do this with VTK and ITK
+    """
+    voxel_center = get_random_voxel(gray_matter_mask, border=False)
+    center_ras = gray_matter_mask.TransformIndexToPhysicalPoint(
+        tuple(voxel_center))
+    r, a, s = center_ras
+    l, p, s = -r, -a, s
+    center_lps = l, p, s
+    with NamedTemporaryFile(suffix='.nii') as reference_file:
+        with NamedTemporaryFile(suffix='.vtp') as model_file:
+            with NamedTemporaryFile(suffix='.nii') as result_file:
+                reference_path = reference_file.name
+                model_path = model_file.name
+                result_path = result_file.name
+
+                # Create noisy sphere
+                poly_data = get_noisy_sphere_poly_data(
+                    center_lps,
+                    radius,
+                )
+                writer = vtk.vtkXMLPolyDataWriter()
+                writer.SetFileName(model_path)
+                writer.SetInputData(poly_data)
+                writer.Write()
+
+                write(resectable_hemisphere_mask, reference_path)  # TODO: use an existing image
+                mesh_to_volume(reference_path, model_path, result_path)
+                sphere_mask = read(result_path)
 
     # Intersection with resectable area
     resection_mask = sitk.And(resectable_hemisphere_mask, sphere_mask)
-
-    if method != 'vtk':
-        # Add some strels on the border
-        resection_mask = add_structural_elements(resection_mask)
 
     # Binary opening to remove leaked CSF from contralateral hemisphere
     if opening_radius is not None:
         resection_mask = sitk.BinaryMorphologicalOpening(
             resection_mask, opening_radius)
 
-    if method != 'vtk':
-        # In case the strels appear outside resectable area
-        resection_mask = sitk.And(resectable_hemisphere_mask, resection_mask)
-
     # Use largest connected component only
     resection_mask = get_largest_connected_component(resection_mask)
 
-    return resection_mask
+    return resection_mask, center_ras
+
+
+def get_slicer_paths(version='4.11'):
+    platform = sys.platform
+    if platform == 'darwin':  # macOS
+        slicer_dir = Path('/Applications/Slicer.app/Contents')
+        slicer_bin_path = slicer_dir / 'MacOS' / 'Slicer'
+    elif platform == 'linux':
+        slicer_dir = Path('~/opt/Slicer/Nightly/Slicer').expanduser()
+        slicer_bin_path = slicer_dir / 'Slicer'
+    rel_path = f'lib/Slicer-{version}/cli-modules/ModelToLabelMap'
+    slicer_module_path = slicer_dir / rel_path
+    return slicer_bin_path, slicer_module_path
 
 
 def mesh_to_volume(reference_path, model_path, result_path):
+    slicer_bin_path, slicer_module_path = get_slicer_paths()
     command = (
-        '/home/fernando/opt/Slicer/Nightly/Slicer',
-        '--launch',
-        '/home/fernando/opt/Slicer/Nightly/lib/Slicer-4.11/cli-modules/ModelToLabelMap',
+        slicer_bin_path,
+        '--launch', slicer_module_path,
         reference_path,
         model_path,
         result_path,
@@ -130,7 +168,7 @@ def add_structural_elements(mask, min_radius=None, max_radius=None, iterations=3
     )
     kernels *= iterations
     num_kernels = len(kernels)
-    radii_kernels = np.random.randint(
+    radii_kernels = torch.randint(
         min_radius,
         max_radius + 1,
         size=(num_kernels, 3),
@@ -163,7 +201,7 @@ def get_largest_connected_component(image):
     return largest_cc
 
 
-def make_noise_image(image_path, parcellation_path, output_path, threshold=True):
+def make_noise_image(image_path, parcellation_path, output_path, threshold=True, fit=False):
     image = read(image_path)
     parcellation = read(parcellation_path)
     csf_mask = get_csf_mask(image, parcellation)
@@ -173,10 +211,16 @@ def make_noise_image(image_path, parcellation_path, output_path, threshold=True)
     if threshold:  # remove non-CSF voxels
         otsu = filters.threshold_otsu(csf_values)
         csf_values = csf_values[csf_values < otsu]
-    distribution = norm  ##
-    args = distribution.fit(csf_values)
-    random_variable = distribution(*args)
-    noise_array = random_variable.rvs(size=image_array.shape).astype(np.float32)
+    if fit:
+        from scipy.stats import rayleigh, rice, norm
+        distribution = norm
+        args = distribution.fit(csf_values)
+        random_variable = distribution(*args)
+        noise_array = random_variable.rvs(
+            size=image_array.shape).astype(np.float32)
+    else:  # assume normal distribution
+        noise_array = torch.FloatTensor(image_array.shape)
+        noise_array.normal_(csf_values.mean(), csf_values.std()).numpy()
     # noise_array = np.random.choice(csf_values, size=image_array.shape)
     noise_image = get_image_from_reference(noise_array, image)
     write(noise_image, output_path)
@@ -196,7 +240,7 @@ def get_random_voxel(mask, border=False):
     array = sitk.GetArrayViewFromImage(image)
     coords = np.array(np.where(array)).T  # N x 3
     N = len(coords)
-    random_index = np.random.randint(N)
+    random_index = torch.randint(N, (1,)).item()
     coords_voxel = coords[random_index]
     coords_voxel = [int(n) for n in reversed(coords_voxel)]  # NumPy vs ITK
     return coords_voxel
@@ -211,7 +255,6 @@ def get_random_voxel_image(mask, border=False):
 
 
 def get_sphere_poly_data(center, radius, theta=None, phi=None):
-    import vtk
     sphere_source = vtk.vtkSphereSource()
     sphere_source.SetCenter(center)
     sphere_source.SetRadius(radius)
@@ -224,28 +267,25 @@ def get_sphere_poly_data(center, radius, theta=None, phi=None):
 
 
 def get_noisy_sphere_poly_data(
-        center,
-        radius,
-        theta=None,
-        phi=None,
-        noise_type=None,
-        octaves=None,
-        smoothness=None,
-        noise_amplitude_radius_ratio=None,
+            center,
+            radius,
+            theta=None,
+            phi=None,
+            noise_type=None,
+            octaves=None,
+            smoothness=None,
+            noise_amplitude_radius_ratio=None,
         ):
     # TODO: apply trsf to sphere to make it an ellipsoid
-    from noise import pnoise3, snoise3
-    import vtk
-    from vtk.util.numpy_support import numpy_to_vtk
-    from vtk.numpy_interface import dataset_adapter as dsa
 
     theta = 64 if theta is None else theta
     phi = 64 if phi is None else phi
     noise_type = 'perlin' if noise_type is None else noise_type
     assert noise_type in 'perlin', 'simplex'
-    octaves = 2 if octaves is None else octaves
-    smoothness = 10 if smoothness is None else smoothness
-    noise_amplitude_radius_ratio = 0.75 if noise_amplitude_radius_ratio is None else noise_amplitude_radius_ratio
+    octaves = 4 if octaves is None else octaves  # make random?
+    smoothness = 10 if smoothness is None else smoothness  # make random?
+    if noise_amplitude_radius_ratio is None:  # make random?
+        noise_amplitude_radius_ratio = 0.75
 
     poly_data = get_sphere_poly_data(center, radius, theta=theta, phi=phi)
     wrap_data_object = dsa.WrapDataObject(poly_data)
@@ -261,7 +301,7 @@ def get_noisy_sphere_poly_data(
     points_with_noise = []
     for point, normal in zip(points, normals):
         x, y, z = point / smoothness
-        noise = function(x, y, z, octaves=octaves)
+        noise = function(x, y, z, octaves=octaves)  # add random offset?
         perturbance = noise_amplitude * noise
         point_with_noise = point + perturbance * normal
         points_with_noise.append(point_with_noise)
@@ -271,30 +311,16 @@ def get_noisy_sphere_poly_data(
     vertices.SetData(numpy_to_vtk(points_with_noise))
     poly_data.SetPoints(vertices)
 
-    normalFilter = vtk.vtkPolyDataNormals()
-    normalFilter.AutoOrientNormalsOn()
-    normalFilter.SetComputePointNormals(True)
-    normalFilter.SetComputeCellNormals(True)
-    normalFilter.SplittingOff()
-    normalFilter.SetInputData(poly_data)
-    normalFilter.Update()
-    poly_data = normalFilter.GetOutput()
+    normals_filter = vtk.vtkPolyDataNormals()
+    normals_filter.AutoOrientNormalsOn()
+    normals_filter.SetComputePointNormals(True)
+    normals_filter.SetComputeCellNormals(True)
+    normals_filter.SplittingOff()
+    normals_filter.SetInputData(poly_data)
+    normals_filter.Update()
+    poly_data = normals_filter.GetOutput()
 
     return poly_data
-
-
-def get_noise_sphere_image(
-        reference,
-        center,
-        radius,
-        theta=None,
-        phi=None,
-        noise_type=None,
-        octaves=None,
-        smoothness=None,
-        noise_amplitude_radius_ratio=None,
-        ):
-    return
 
 
 def get_sphere_mask(voxel_image, radius, approximate=True):
@@ -358,7 +384,8 @@ def get_csf_mask(image, parcellation, erode_radius=1):
     progress = tqdm(lines, leave=False)
     for line in progress:
         line = line.lower()
-        if 'periventricular' in line: continue
+        if 'periventricular' in line:
+            continue
         if not 'ventric' in line.lower():
             label, name = line.split()[:2]
             label = int(label)
@@ -420,92 +447,71 @@ def write(image, image_path):
     sitk.WriteImage(image, str(image_path))
 
 
-if __name__ == "__main__":
-    np.random.seed(42)
-    # input_path = Path('~/Dropbox/MRI/t1.nii.gz').expanduser()
-    # # brain_segmentation_path = Path('~/Dropbox/MRI/t1_brain_seg.nii.gz').expanduser()
-    # parcellation_path = Path('~/Dropbox/MRI/t1_seg_gif.nii.gz').expanduser()
-    # input_path = '/home/fernando/episurg/mri/mni_alejandro/MNI_152_mri.nii.gz'
-    # parcellation_path = '/home/fernando/episurg/mri/mni_alejandro/MNI_152_gif.nii.gz'
+def get_resection_mask_old(
+    resectable_hemisphere_mask,
+    gray_matter_mask,
+    radius,
+    opening_radius=None,
+    method='vtk',
+):
 
-    output_dir = Path('/tmp/resected_perlin')
-    output_dir.mkdir(exist_ok=True)
-    # if output_dir.is_dir():
-    #     import shutil
-    #     shutil.rmtree(output_dir)
+    voxel_image = get_random_voxel_image(gray_matter_mask, border=False)
+    sphere_mask = get_sphere_mask(voxel_image, radius)
 
-    N = 10
+    # Intersection with resectable area
+    resection_mask = sitk.And(resectable_hemisphere_mask, sphere_mask)
 
-    input_path = '/tmp/1395_unbiased.nii.gz'
-    parcellation_path = '/home/fernando/episurg_old/subjects/1395/mri/t1_pre/assessors/1395_t1_pre_gif_seg.nii.gz'
-    noise_image_path = Path('/tmp/1395_unbiased_noise.nii.gz')
-    if not noise_image_path.is_file():
-        make_noise_image(input_path, parcellation_path, noise_image_path)
-    for i in trange(N):
-        output_path = output_dir / f'1395_resected_{i}.nii.gz'
-        resection_mask_output_path = output_dir / f'1395_resected_{i}_label.nii.gz'
-        hemisphere = 'left' if np.random.rand() > 0.5 else 'right'
-        opening_radius = 3
-        resect(
-            input_path,
-            parcellation_path,
-            noise_image_path,
-            output_path,
-            resection_mask_output_path,
-            hemisphere,
-            opening_radius=opening_radius,
-        )
+    if method != 'vtk':
+        # Add some strels on the border
+        resection_mask = add_structural_elements(resection_mask)
+
+    # Binary opening to remove leaked CSF from contralateral hemisphere
+    if opening_radius is not None:
+        resection_mask = sitk.BinaryMorphologicalOpening(
+            resection_mask, opening_radius)
+
+    if method != 'vtk':
+        # In case the strels appear outside resectable area
+        resection_mask = sitk.And(resectable_hemisphere_mask, resection_mask)
+
+    # Use largest connected component only
+    resection_mask = get_largest_connected_component(resection_mask)
+
+    return resection_mask
 
 
+# if __name__ == "__main__":
+#     np.random.seed(42)
+#     # input_path = Path('~/Dropbox/MRI/t1.nii.gz').expanduser()
+#     # # brain_segmentation_path = Path('~/Dropbox/MRI/t1_brain_seg.nii.gz').expanduser()
+#     # parcellation_path = Path('~/Dropbox/MRI/t1_seg_gif.nii.gz').expanduser()
+#     # input_path = '/home/fernando/episurg/mri/mni_alejandro/MNI_152_mri.nii.gz'
+#     # parcellation_path = '/home/fernando/episurg/mri/mni_alejandro/MNI_152_gif.nii.gz'
 
+#     output_dir = Path('/tmp/resected_perlin')
+#     output_dir.mkdir(exist_ok=True)
+#     # if output_dir.is_dir():
+#     #     import shutil
+#     #     shutil.rmtree(output_dir)
 
+#     N = 10
 
-
-
-
-
-
-
-
-    # sphere_path = '/tmp/noisy_sphere.vtp'
-    # center = -37, 38, -24
-    # radius = 30
-    # pd = get_noisy_sphere_poly_data(
-    #     center,
-    #     radius,
-    # )
-    # import vtk
-    # writer = vtk.vtkXMLPolyDataWriter()
-    # writer.SetFileName(sphere_path)
-    # writer.SetInputData(pd)
-    # writer.Write()
-    # # m = vtk.vtkVoxelModeller()
-    # # m.SetScalarTypeToChar()
-    # # m.SetInputData(pd)
-    # # output = m.GetOutput()
-
-    # ref = read(input_path)
-    # white_image = vtk.vtkImageData()
-    # white_image.SetSpacing(ref.GetSpacing())
-    # white_image.SetDimensions(ref.GetSize())
-    # white_image.SetOrigin(ref.GetOrigin())
-
-    # pol2stenc = vtk.vtkPolyDataToImageStencil()
-    # pol2stenc.SetInputData(pd)
-    # pol2stenc.SetOutputOrigin(ref.GetOrigin())
-    # pol2stenc.SetOutputSpacing(ref.GetSpacing())
-    # extent = list(zip((0, 0, 0), ref.GetSize()))
-    # extent = np.array(extent).flatten()
-    # pol2stenc.SetOutputWholeExtent(extent)
-    # pol2stenc.Update()
-
-    # import itk
-    # Dimension = 2
-    # PixelType = itk.UC
-    # ImageType = itk.Image[PixelType, Dimension]
-    # vtkToItkFilter = itk.VTKImageToImageFilter[ImageType].New()
-    # vtkToItkFilter.SetInput(pol2stenc.GetOutput())
-
-    # vtkToItkFilter.Update()
-    # myitkImage = vtkToItkFilter.GetOutput()
-    # myitkImage.SetDirection(ref.GetDirection())
+#     input_path = '/tmp/1395_unbiased.nii.gz'
+#     parcellation_path = '/home/fernando/episurg_old/subjects/1395/mri/t1_pre/assessors/1395_t1_pre_gif_seg.nii.gz'
+#     noise_image_path = Path('/tmp/1395_unbiased_noise.nii.gz')
+#     if not noise_image_path.is_file():
+#         make_noise_image(input_path, parcellation_path, noise_image_path)
+#     for i in trange(N):
+#         output_path = output_dir / f'1395_resected_{i}.nii.gz'
+#         resection_mask_output_path = output_dir / f'1395_resected_{i}_label.nii.gz'
+#         hemisphere = 'left' if np.random.rand() > 0.5 else 'right'
+#         opening_radius = 3
+#         resect(
+#             input_path,
+#             parcellation_path,
+#             noise_image_path,
+#             output_path,
+#             resection_mask_output_path,
+#             hemisphere,
+#             opening_radius=opening_radius,
+#         )
