3b8f2ca411f42c4f2c79ac9dc5693135e3d7054c
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2021-02-04 17:02:01 +0000

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/resector',
-    version='0.2.5',
+    version='0.2.6',
     zip_safe=False,
 )
